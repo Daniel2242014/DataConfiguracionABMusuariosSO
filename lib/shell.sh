bb078753fbeea5de04c68589f88393e6533b8642
root <root@192.168.1.48>
2019-06-10 01:10:40 -0300

    Subida de informacion v1

---
+++ lib/shell.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+# VERCION 1.0 - 25/6 PRIMERA ENTREGA desarrolado por Bit (3°BD 2019)
+
+cambiarShell() #encargado de verificar y devolver una direcion de un shell para un usuario, al poder tener muchos tipos de shell y estos al ser todos archivos regulares no hay muchas forma de verificar que realmente se trate de un shell 
+{
+verif=0
+while test $verif -eq 0
+do
+	echo "Ingrese la direcion del shell de inicio (se usara el por defecto si no ingresa nada)" 	
+	read dato #Ingreso de informacion
+	if test -z $dato 
+	then
+		respuesta="POR DEFECTO" #salida por defecto
+		verif=1 #rompe el bucle 		
+		
+	else
+		if test -f $dato #verifica que la ruta del archivo exista y ademas sea un archivo regular 
+		then
+			respuesta=$dato #salida de la informacion
+			verif=1
+		else
+			error "Ese archivo no existe "
+		fi
+	fi
+done
+}
+
+mostrarShell()
+{
+	respuesta=$(grep -e "^$1:" '/etc/passwd'| cut -d: -f7)
+}
---
+++ setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+if test $USER = "root"
+then
+
+clear
+
+source ./lib/lib_menu.sh
+source lib/lib_error.sh
+source lib/DT.sh
+source lib/expiracionUsuario.sh
+source lib/GP.sh
+source lib/GS.sh
+source lib/NDiasHasta.sh
+source lib/pass.sh
+source lib/shell.sh
+source lib/UDI.sh
+source lib/userE.sh
+source lib/fechacal.sh
+source ./sub_shell/agregarUsuario.sh 
+source ./sub_shell/ModificarUsuario.sh 
+source ./sub_shell/eliminarUsuario.sh
+source ./sub_shell/listarUsuarios.sh 
+source ./sub_shell/agregarGrupo.sh 
+source ./sub_shell/ModificarGrupo.sh 
+source ./sub_shell/EliminarGrupo.sh 
+source ./sub_shell/listarGrupos.sh 
+source ./sub_shell/Preferencias.sh 
+source ./sub_shell/ConfiguracionDelAmbienteDeTrabajo.sh 
+
+respuesta="" #El dato pasado por los return solo puede ser numerico, entonces utilizamos una variable externa donde se cargen las salidas, como si fuera un $? pero con mayor capasidad 
+#respuestaA=() #Lo mismo que en respuesta solo que para arrays
+
+echo "   _____________________________________________  "
+echo "   |                                           | "
+echo "   |                                           | "
+echo "   |           ABM usuarios y grupos           | "
+echo "   |                  por Bit                  | "
+echo "   |                                           | "
+echo "   |___________________________________________| "
+echo "" 
+		
+nombres=('Agregar_usuario' 'Modificar_usuarios' 'Eliminar_usuarios' 'Listar_usuarios' 'Agregar_grupo' 'editar_grupo' 'eliminar_grupo' 'Listar_grupo' 'Editar_preferencias' 'Instalar')
+
+direcionesSetUp=('agregarUsuario' 'ModificarUsuario' 'eliminarUsuarios' 'listarUsuarios' 'agregarGrupo' 'ModificarGrupo' 'eliminarGrupo' 'MenuListarGrupos' 'Preferencias' 'ConfiguracionDelAmbienteDeTrabajo')
+
+menu 'nombres[@]' 'direcionesSetUp[@]'
+
+else
+	echo "Debe ser root para ejecutar este software"
+fi
