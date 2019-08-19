57170ee7fd6c32e42f4ffc3612b1604f4db820de
Kouta <darkfm@vera.com.uy>
2019-08-19 06:03:46 -0300

    Automatización de la instalación de informix parte namber uan

---
+++ post-inst.2.sh
@@ -0,0 +1,12 @@
+#P2
+
+sh informix_postinstall2.sh
+source setup.sh
+
+echo -n "Desea configurar el entorno de red? (Y/n): "
+read sel
+if [ "$sel" != "n" ]
+then
+	source sub_shell/configurarRed.sh
+	configurarRed
+fi
