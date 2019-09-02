db40b454bc77f811f69a564d6ef058ce002b1a50
Daniel <Daniel2242014@gmail.com>
2019-09-02 11:10:22 -0300

    Carga 2

--- sub_shell/uninstall.sh
+++ sub_shell/uninstall.sh
@@ -33,44 +33,40 @@ desinstalar()
 		fi
 		echo "\S" > /etc/issue
 		echo "Kernel \r on an \m" >> /etc/issue	#Cargamos el contenido por defecto 
-		if test -z $1
 		sed -i '/0 * * * * root bkupScript.sh/d' /etc/crontab
 		sed -i '/0 0 * * * root logrotate /etc/logrot.cfg/d' /etc/crontab
-		
-		
 		if test -f /opt/IBM
 		then
 		#ELIMINACION de informix 
-		echo "Usted desea eliminar Informix [1=si 0=no]"
-		read d 
-		case $d in 
-		1)
-			sed -i '/sqlexec\|sqlturbo/d' /etc/services
-			sed -i '/vmInformix/d' /etc/hostname
-			sed -i '/192.168.1.100 vmInformix/d' /etc/hosts
+			echo "Usted desea eliminar Informix [1=si 0=no]"
+			read d 
+			case $d in 
+			1)
+				sed -i '/sqlexec\|sqlturbo/d' /etc/services
+				sed -i '/vmInformix/d' /etc/hostname
+				sed -i '/192.168.1.100 vmInformix/d' /etc/hosts
 
-			rm -rf /etc/profile.d/zz_configInformix.sh
-			echo "Cargando...."
-			/opt/IBM/Informix_Software_Bundle/uninstall/uninstall_server/uninstallserver -i console
-			echo "Esperando a que termine eliminacion en segundo plano"
-			sleep 3
-			rm -rf /opt/IBM
-			userdel informix
-			groupdel informix
+				rm -rf /etc/profile.d/zz_configInformix.sh
+				echo "Cargando...."
+				/opt/IBM/Informix_Software_Bundle/uninstall/uninstall_server/uninstallserver -i console
+				echo "Esperando a que termine eliminacion en segundo plano"
+				sleep 3
+				rm -rf /opt/IBM
+				userdel informix
+				groupdel informix
 
-		;;
+			;;
 
-		*)
-		echo "No se eliminara"
-		;;
-		
-		esac
-	fi
-	if test -z $1
-	then 
-		echo "Proseso terminado con exito"	
-		read f
-		verifMenu=-1
-	fi
-
+			*)
+			echo "No se eliminara"
+			;;
+			
+			esac
+		fi
+		if test -z $1
+		then 
+			echo "Proseso terminado con exito"	
+			read f
+			verifMenu=-1
+		fi
 }
