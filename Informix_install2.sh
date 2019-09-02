af14a9c874f0ac9749db9d4266298b8e215130a2
Daniel2242014 <40508304+Daniel2242014@users.noreply.github.com>
2019-09-02 15:35:31 -0300

    subir service informix

--- Informix_install2.sh
+++ Informix_install2.sh
@@ -4,6 +4,8 @@ su informix -c "source /etc/profile"
 su informix -c "oninit -ivy"
 su informix -c "onmode -vky"
 su informix -c "oninit -vy"
+cp informix.service /etc/systemd/system/
+cp sysconfig.informix /etc/sysconfig/informix
 systemctl start informix
 systemctl enable informix
 rm -f /var/DataConfiguracionABMusuariosSO/I_Inxo
