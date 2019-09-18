a3147b1b9bf2c2b546889dea1bbcdd4e78f1bd41
root <root@localhost.localdomain>
2019-09-18 02:57:34 -0300

    Correcion en setup.sh

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ ConfiguracionDelAmbienteDeTrabajo() #Funcion encarga de la instalacion
 		chmod u+x /sbin/bkupScript.sh
 		sed -i '/0 0 \* \* \* root bkupScript.sh/d' /etc/crontab
 		sed -i '/0 0 \* \* \* root logrotate.*/d' /etc/crontab
-		#echo "0 * * * * root bkupScript.sh" >> /etc/crontab
+		echo "0 * * * * root bkupScript.sh" >> /etc/crontab
 		cat > /etc/logrot.cfg <<EOF
 /var/log/messages {
 	rotate 4
@@ -51,7 +51,7 @@ ConfiguracionDelAmbienteDeTrabajo() #Funcion encarga de la instalacion
 	endscript
 }
 EOF
-		#echo "0 0 * * * root logrotate /etc/logrot.cfg" >> /etc/crontab
+		echo "0 0 * * * root logrotate /etc/logrot.cfg" >> /etc/crontab
 		echo "" > /etc/ssh/allowed
 		#Subido en la direcion url que se puede ver en la linea anterior se tiene subido todos los shell script y funciones nesesarias para el correcto funcionamiento de la ABM. De esta forma el usuario no debera tener todos los archivos, solamente el shell setup para la instalacion
 		mv /var/DataConfiguracionABMusuariosSO/Titular.sh /etc/profile.d/Titular.sh #Mueve el titular a profile.d, de esta forma se ejecuta al inicio del sistema
