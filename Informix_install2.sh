27f46809b50a412fc4208be08c223d704f98f202
kouta-kun <44276987+kouta-kun@users.noreply.github.com>
2019-09-21 11:48:40 -0300

    Update Informix_install2.sh

--- Informix_install2.sh
+++ Informix_install2.sh
@@ -4,7 +4,6 @@ source /etc/profile.d/zz_configInformix.sh
 su informix -c "source /etc/profile.d/zz_configInformix.sh; oninit -ivy; onmode -vky; oninit -vy"
 cp /var/DataConfiguracionABMusuariosSO/informix.service /etc/systemd/system/
 cp /var/DataConfiguracionABMusuariosSO/sysconfig.informix /etc/sysconfig/sysconfig.informix
-systemctl start informix
 systemctl enable informix
 rm -f /var/DataConfiguracionABMusuariosSO/I_Inxo
 echo "Informix instalado con exito"
