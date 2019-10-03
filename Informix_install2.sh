c7c3a71a5408d0bef8d82d8ddeb1116cea5f61d5
Daniel2242014 <40508304+Daniel2242014@users.noreply.github.com>
2019-10-03 16:11:24 -0300

    Update Informix_install2.sh

--- Informix_install2.sh
+++ Informix_install2.sh
@@ -6,6 +6,12 @@ cp /var/DataConfiguracionABMusuariosSO/informix.service /etc/systemd/system/
 cp /var/DataConfiguracionABMusuariosSO/sysconfig.informix /etc/sysconfig/sysconfig.informix
 #systemctl start informix
 systemctl enable informix
+echo "Modificado firewall"
+# puede entrar a la red todo lo que venga por Informix (9088)
+		iptables -A INPUT -p tcp --destination-port 9088 -j ACCEPT
+		iptables -A OUTPUT -p tcp --destination-port 9088 -j ACCEPT
+		iptables -A INPUT -p udp --destination-port 9088 -j ACCEPT
+		iptables -A OUTPUT -p udp --destination-port 9088 -j ACCEPT
 source /var/DataConfiguracionABMusuariosSO/lib/re_sysmaster.sh
 sysmaster '0'
 rm -f /var/DataConfiguracionABMusuariosSO/I_Inxo
