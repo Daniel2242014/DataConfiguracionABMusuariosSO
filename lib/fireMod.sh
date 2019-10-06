b84d962b0945e8eaa15a690e617a11a302fce4fd
Daniel <Daniel2242014@gmail.com>
2019-10-06 00:46:39 -0300

    ejej

--- lib/fireMod.sh
+++ lib/fireMod.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 #VERCION 3.0 - 25/10 TERCERA ENTREGA desarrolado por Bit (3°BD 2019)
 
-fireMod1()
+fireMod1() 
 {
  fluchFire
  politD
  iptables -A INPUT -p tcp -m tcp --dport 20022 -j ACCEPT #PErmitimos entrada del ssh (Se cambio el puerto) 
  iptables -A OUTPUT -p tcp -m tcp --sport 20022 -m state --state ESTABLISHED,RELATED -j ACCEPT
- echo "1" > /var/DataConfiguracionABMusuariosSO/fire.data
- service iptables save
+ if test  $1 -eq 1 2>/dev/null
+ then 
+   return   
+ fi   
+  echo "1" > /var/DataConfiguracionABMusuariosSO/fire.data
+  service iptables save
 }
 
 fireMod2()
@@ -19,6 +23,10 @@ fireMod2()
  iptables -A OUTPUT -p tcp -m tcp --sport 20022 -m state --state ESTABLISHED,RELATED -j ACCEPT
  iptables -A INPUT -p tcp -m tcp --dport 9088 -j ACCEPT #PErmitimos entrada del ODBC (INFORMIX)
  iptables -A OUTPUT -p tcp -m tcp --sport 9088 -m state --state ESTABLISHED,RELATED -j ACCEPT
+ if test  $1 -eq 1 2>/dev/null
+ then 
+   return   
+ fi  
  echo "2" > /var/DataConfiguracionABMusuariosSO/fire.data
  service iptables save
 }
@@ -27,6 +35,10 @@ fireMod0()
 {
  fluchFire
  politA	 
+ if test  $1 -eq 1 2>/dev/null
+ then 
+   return   
+ fi  
  echo "0" > /var/DataConfiguracionABMusuariosSO/fire.data
  service iptables save
 
